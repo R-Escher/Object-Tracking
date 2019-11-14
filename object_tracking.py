784e73f8702635d78b4a5806cd4c75f012606021
Rafael Escher <38119826+R-Escher@users.noreply.github.com>
2019-11-13 22:33:37 -0300

    added parser argument to select video

--- object_tracking.py
+++ object_tracking.py
@@ -7,36 +7,28 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 # argument parser
-if len(sys.argv) == 4:
-    kp_detector = sys.argv[1]
-    numb_of_kp = int(sys.argv[2])
-    matchPercent = float(sys.argv[3])
+if len(sys.argv) == 5:
+    video = sys.argv[1]
+    kp_detector = sys.argv[2]
+    numb_of_kp = int(sys.argv[3])
+    matchPercent = float(sys.argv[4])
 
     if kp_detector == "sift":
         kp_detector = cv.xfeatures2d.SIFT_create(numb_of_kp)
+        # sets BFM recommended configuration for sift
+        bf = cv.BFMatcher(cv.NORM_L2,crossCheck=True)
     elif kp_detector == "surf":
         kp_detector = cv.xfeatures2d.SURF_create(numb_of_kp)
+        # sets BFM recommended configuration for surf
+        bf = cv.BFMatcher(cv.NORM_L2,crossCheck=True)        
     elif kp_detector == "orb":
         kp_detector = cv.ORB_create(numb_of_kp)
+        # sets BFM recommended configuration for surf
+        bf = cv.BFMatcher(cv.NORM_HAMMING,crossCheck=True)           
 else:
     print("Number of arguments must be 3:\n  1. Type of KeyPoint Detector and Descriptor Extractor\n  2. Number of KeyPoints to be found (0 sets standard number) \n  3. Percentage of good matches to be considered")
     sys.exit()
 
-
-
-
-
-
-
-
-
-
-
-
-
-# creates kp_detector
-
-#kp_detector = cv.ORB_create()
 
 
 # loads train image into grayscale
@@ -44,7 +36,7 @@ train_Img = cv.imread('book_train.jpg', cv.IMREAD_GRAYSCALE)
 
 
 # loads test video
-cap = cv.VideoCapture('book2.mp4')
+cap = cv.VideoCapture(str(video))
 
 
 # resizes images to fit screen
@@ -57,8 +49,6 @@ print("Computing train image...")
 train_KP, train_Desc = kp_detector.detectAndCompute(train_Img,None)
 
 
-# declares use Brute-Force Matcher
-bf = cv.BFMatcher(cv.NORM_L2,crossCheck=True)
 cont = 0
 plt.show()
 
@@ -69,7 +59,8 @@ while True:
     #test_Img = cv.cvtColor(test_Img, cv.COLOR_BGR2GRAY)
     
     # converts from bgr to rgb due to mpl bug
-    test_Img = cv.cvtColor(test_Img, cv.COLOR_BGR2RGB)
+    #test_Img = cv.cvtColor(test_Img, cv.COLOR_BGR2RGB)
+    test_Img = cv.cvtColor(test_Img, cv.COLOR_BGR2GRAY)
     test_Img =  cv.resize(test_Img, (2400, 1400), interpolation = cv.INTER_AREA)
 
     # detects and computes test images
@@ -89,8 +80,7 @@ while True:
     train_pts = np.float32([train_KP[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
 
     # Find homography
-    h, mask = cv.findHomography( train_pts, query_pts, cv.RANSAC)        
-    matches_mask = mask.ravel().tolist()
+    h, _ = cv.findHomography( train_pts, query_pts, cv.RANSAC)        
 
     # Use homography
     height, width = train_Img.shape
@@ -101,9 +91,8 @@ while True:
     homography = cv.polylines(test_Img, [np.int32(dst)], True, (255, 0, 0), 3)
 
     # Draw best matches.
-    img3 = cv.drawMatches( test_Img, test_KP,train_Img, train_KP, matches, None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    img3 = cv.drawMatches( homography, test_KP,train_Img, train_KP, matches, None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     plt.figure(1); plt.clf()
-    plt.imshow(homography)
     plt.imshow(img3)
     plt.pause(0.0000001)
 
